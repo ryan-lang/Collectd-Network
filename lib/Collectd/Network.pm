8f3bc0d786959e95065b166b33bcef6050d052e1
Ryan Lang <rlang@me.com>
2017-04-02 20:53:10 -0700

    initial commit

---
+++ lib/Collectd/Network.pm
@@ -0,0 +1,20 @@
+package Collectd::Network;
+# ABSTRACT: Collectd network protocol implementation
+
+use strict;
+use warnings;
+ 
+our $VERSION = "0.01";
+ 
+
+ 
+1;
+__END__
+
+=encoding utf-8
+ 
+=head1 AUTHOR
+ 
+Ryan Lang <rlang@me.com>
+ 
+=cut
